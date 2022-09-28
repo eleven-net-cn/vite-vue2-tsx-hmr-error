ce6f0c62267161fa0af5310ce705e6bcbbb029dc
Eleven <jun.xi@mihoyo.com>
2022-09-28 20:14:03 +0800

    test: test url-loader

---
+++ react-app-ts-v4_template/config/webpackDevServer.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const paths = require('./paths');
+const packageJson = require('../package');
+
+module.exports = function () {
+  const publicPath = process.env.PUBLIC_URL || '/';
+
+  return {
+    publicPath,
+    contentBase: paths.appPubic,
+    contentBasePublicPath: publicPath,
+    watchContentBase: true,
+    inline: true,
+    hot: true,
+    open: false,
+    compress: true,
+    disableHostCheck: false,
+    transportMode: 'ws',
+    quiet: false,
+    overlay: true,
+    clientLogLevel: 'none',
+    stats: 'errors-warnings',
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    proxy: packageJson.proxy,
+    before(app) {},
+  };
+};
---
+++ react-app-ts-v4_template/src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import './app.less';
+
+const App: FC = () => {
+  return (
+    <>
+      <div className="hello-wrapper">Hello World</div>
+    </>
+  );
+};
+
+export default App;
