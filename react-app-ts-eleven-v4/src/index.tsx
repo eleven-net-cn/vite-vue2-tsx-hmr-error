404eda46b172fad4ffd80ee79ec8c26dfa49d185
Eleven <jun.xi@mihoyo.com>
2022-11-21 20:13:47 +0800

    chore: add vite-demo-vue2

--- react-app-ts-eleven-v4/src/index.tsx
+++ react-app-ts-eleven-v4/src/index.tsx
@@ -8,6 +8,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
--- vite-demo-example-02/env.d.ts
+++ vite-demo-example-02/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    /** 来自wrapper的minxin，提供事件能力 */
+    // localEvent: LocalEvent;
+    /** pzLoader，提供预加载能力 */
+    pzLoader?: {
+      load: (param: { urlList: string[] }) => void;
+    };
+    $pzConfig: string;
+  }
+}
