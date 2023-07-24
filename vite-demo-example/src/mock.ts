646f31b7185e0b4e412c6483453b57f0696fbd77
Eleven <jun.xi@mihoyo.com>
2023-07-24 16:20:00 +0800

    chore: rm useless projects

--- vite-demo-example/src/mock.ts
+++
@@ -1,9 +0,0 @@
-import Mock from 'mockjs-async';
-
-Mock.mock(/\/mock\/31\/in\/v1\/app\/list/, () => {
-  return {
-    retcode: 0,
-    message: 'success',
-    data: {},
-  };
-});
--- vite-demo-example/src/xhr.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function queryList() {
-  return axios
-    .get('https://plat-yapi.mihoyo.com/mock/31/in/v1/app/list')
-    .then(({ data }) => {
-      return data;
-    });
-}
